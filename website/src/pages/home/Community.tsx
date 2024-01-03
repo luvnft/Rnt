352ec3972eb47bb6eb62ba01e3c4491ba797e786
luv nft <info@blkluv.org>
2024-01-03 15:40:32 -0500

    updated tg

--- website/src/pages/home/Community.tsx
+++ website/src/pages/home/Community.tsx
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>Twitter and Telegram Widgets</title>
-  <script src="https://platform.twitter.com/widgets.js" async charset="utf-8"></script>
-  <script src="https://telegram.org/js/telegram-widget.js?22" async></script>
-</head>
-<body>
-  <div>
-    <TwitterList />
-    <TelegramList />
-  </div>
+import { useEffect, useRef } from 'react'
 
-  <script>
-  function TwitterList() {
+export function TwitterList() {
   const xc = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
@@ -29,7 +16,7 @@
   return (
     <div>
       <h1 className="my-[20px] flex items-center justify-between text-[24px] font-[500] leading-[30px]">
-        Twitter
+        X
       </h1>
       <div ref={xc} className="">
         <a
@@ -46,32 +33,28 @@
   )
 }
 
-    function TelegramList() {
-      const tc = useRef<HTMLDivElement>(null)
+export function TelegramList() {
+  const tc = useRef<HTMLDivElement>(null);
 
-      useEffect(() => {
-        if (tc.current) {
-          const script = document.createElement('script')
-          script.src = 'https://telegram.org/js/telegram-widget.js?22'
-          script.setAttribute('data-telegram-post', 'rntsat')
-          script.setAttribute('data-comments-limit', '20')
-          script.setAttribute('data-dark', '1')
-          script.setAttribute('data-height', '1000')
-          tc.current.appendChild(script)
-        }
-      }, [])
-
-      return (
-        <div>
-          <h1 className="my-[20px] flex items-center justify-between text-[24px] font-[500] leading-[30px]">
-            Telegram
-          </h1>
-          <div ref={tc} className="">
-            <a href="https://t.me/rntsat"></a>
-          </div>
-        </div>
-      )
+  useEffect(() => {
+    if (tc.current) {
+      const script = document.createElement('script');
+      script.src = 'https://telegram.org/js/telegram-widget.js?22';
+      script.setAttribute('data-telegram-discussion', 'rntsat');
+      script.setAttribute('data-comments-limit', '20');
+      script.setAttribute('data-dark', '1');
+      script.setAttribute('data-height', '1000');
+      tc.current.appendChild(script);
     }
-  </script>
-</body>
-</html>
+  }, []);
+
+  return (
+    <div>
+      <h1 className="my-[20px] flex items-center justify-between text-[24px] font-[500] leading-[30px]">
+        Telegram
+      </h1>
+      <div ref={tc}></div>
+      <a href="https://t.me/rntsat">Join our RNTal Telegram market</a>
+    </div>
+  );
+}
